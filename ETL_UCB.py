8cd7babbf8e4fd25cf697ddebecdf9e03500fb0b
andrescamperoc <andres.campero@ucb.edu.bo>
2024-05-25 01:39:20 +0000

    etl viernes

--- ETL_UCB.py
+++ ETL_UCB.py
@@ -10,8 +10,8 @@ wine_quality_url = "https://archive.ics.uci.edu/ml/machine-learning-databases/wi
 wine_quality_url = pd.read_csv(wine_quality_url, sep=";")
 
 #initial look at the data
-print(wine_data.head())
-print(wine_quality_url.head())
+#print(wine_data.head())
+#print(wine_quality_url.head())
 
 #Transformacion
 #Agregando columnas al cvs
@@ -22,9 +22,28 @@ wine_data.columns =["class", "alcohol", "malic acid", "ash",
                     "color intensity", "hue", "OD280/OD315 of diluted wines",
                     "proline"]
 
+#converting Class columns into categorical datatype
+wine_data["class"] = wine_data["class"].astype("category")
+
+#Checking for any missing values in both datasets
+print(wine_data.isnull().sum())
+print(wine_quality_url.isnull().sum())
+
+#normalizando acohol
+wine_data["alcohol"]=(wine_data["alcohol"]-wine_data["alcohol"].min())/
+(wine_data["alcohol"].max()-wine_data["alcohol".min())
+
+wine_data['alcohol'] = wine_data['alcohol'] * 10000
+
+# Creating an average quality column in wine_quality_data
+wine_quality_data['average_quality'] = wine_quality_data[['fixed acidity', 'volatile acidity', 'citric acid',
+                                                          'residual sugar', 'chlorides', 'free sulfur dioxide',
+                                                          'total sulfur dioxide', 'density', 'pH', 'sulphates',
+                                                          'alcohol']].mean(axis = 1)
 
 #Saving the transformed data as csv file
-
 wine_data.to_csv("wine_dataset.csv", index=False)
 wine_quality_url.to_csv('wine_quality_dataset.csv', index = False)
+
+
 
